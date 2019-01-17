d71912acf3b1972c540cd8eb227c9e72c34631a4
Philipp Kraus <philipp.kraus@flashpixx.de>
2019-01-18 00:33:03 +0100

    routes

--- main.py
+++ main.py
@@ -8,15 +8,37 @@ from microWebSrv import MicroWebSrv
 
 # --- server routes ---
 
-@MicroWebSrv.route('/hello-world')
-def route_hellow_world(client, response) :
-    print('Hello World...')
-    response.WriteResponseOk()
-
-
 @MicroWebSrv.route('/reset')
 def route_reset(client, response) :
     machine.reset()
+
+
+@MicroWebSrv.route('/info')
+def route_info(client, response) :
+    import ubinascii
+    import gc
+
+    net = network.WLAN(network.STA_IF)
+    response.WriteResponseJSONOk(
+        {
+            'machine': {
+                'frequence_mhz': machine.freq() / 1000000,
+                'id': ubinascii.hexlify(machine.unique_id(), ':').decode(),
+                'free_mem_kb': gc.mem_free() / 1000
+            },
+            'net': {
+                'hostname': net.config('dhcp_hostname'),
+                'mac': ubinascii.hexlify(net.config('mac'), ':').decode(),
+                'ssid': net.config('essid'),
+                'ip': net.ifconfig()[0],
+                'netmask': net.ifconfig()[1],
+                'router': net.ifconfig()[2],
+                'dns': net.ifconfig()[3]
+            }
+        }
+    )
+
+
 
 
 
